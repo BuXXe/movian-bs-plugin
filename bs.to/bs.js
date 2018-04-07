a795a975063419a0eb412e030429476d353c3951
LFriede <gORDon_vdLg@gmx.de>
2018-04-07 16:29:42 +0200

    added page.loading animation

--- bs.to/bs.js
+++ bs.to/bs.js
@@ -109,6 +109,7 @@
   plugin.addURI(PLUGIN_PREFIX + ":ShowHostsForEpisode:(.*)", function(page,episodeLink){
 	  page.metadata.icon = Plugin.path + 'bs.png';
 	  page.type = 'directory';
+    page.loading = true;
 	  // get the series title, season and episode number
 	  // seasonlink is serie/seriesname/seasonnumber/episodename
 	  page.metadata.title = episodeLink.split("/")[1] + " - Season "+episodeLink.split("/")[2]+ " - Episode "+episodeLink.split("/")[3];
@@ -140,6 +141,8 @@
         }
       }
     }
+
+    page.loading = false;
   });
 
   // Lists the available episodes for a given season
@@ -149,6 +152,7 @@
 	  // get the series title and season
 	  // seasonlink is serie/seriesname/seasonnumber
 	  page.metadata.title = seasonLink.split("/")[1] + " - Season "+seasonLink.split("/")[2];
+    page.loading = true;
 
 	  var SeasonResponse = showtime.httpGet("http://bs.to/"+seasonLink);
 	  var dom = html.parse(SeasonResponse.toString());
@@ -179,12 +183,14 @@
 			  title: "Episode " + episodeNumber + " " + Titles
 		  });
 	  }
+
+    page.loading = false;
   });
 
   // Series Handler: show seasons for given series link
   plugin.addURI(PLUGIN_PREFIX + ':SeriesSite:(.*)', function(page, series) {
 	  	page.metadata.icon = Plugin.path + 'bs.png';
-	  	page.loading = false;
+	  	page.loading = true;
 	  	page.type = 'directory';
 	  	page.metadata.title = series.split("serie/")[1];
 
@@ -209,6 +215,7 @@
 	  	page.metadata.icon = Plugin.path + 'bs.png';
 	  	page.type = "directory";
 	    page.metadata.title = "bs.to series list";
+      page.loading = true;
 
 	  	var BrowseResponse = showtime.httpGet("http://bs.to/serie-alphabet");
 	  	var dom = html.parse(BrowseResponse.toString());
@@ -235,6 +242,8 @@
     			store.favorites = showtime.JSONEncode(obj);
     		});
 	    }
+
+      page.loading = false;
   });
 
 
@@ -270,10 +279,11 @@
         obj.push({link:streamLink, title:title});
         store.favorites = showtime.JSONEncode(obj);
       });
-
-      if(noEntry == true)
-        page.appendPassiveItem('video', '', { title: 'The search gave no results' });
     }
+
+    if(noEntry == true)
+      page.appendPassiveItem('video', '', { title: 'The search gave no results' });
+
     page.loading = false;
   });
 
